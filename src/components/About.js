13ff2d990e2dc2e43afec201e52eab5b7bc4f8f7
Mahadev1729 <devcampus17@gmail.com>
2024-12-19 16:21:39 +0530

    rest

--- src/components/About.js
+++ src/components/About.js
@@ -1,41 +1,20 @@
-import React from 'react'
-import User from "./User"
-import UserContext from '../utils/UserContext';
+import React, { useContext } from "react";
+import User from "./User";
+import UserContext from "../utils/UserContext";
 
+const About = () => {
+  const { loggedInUser } = useContext(UserContext);
 
-class About extends React.Component{
-  constructor(props){
-    super(props);
-    // console.log( " Parent Constructor")
-  }
-
-  componentDidMount(){
-  //   console.log("Parent Component Mount did");
-  }
-  render()
-  {
-     
-    // console.log(+"Parent Render");
-    return (
+  return (
+    <div>
+      <h1>About</h1>
       <div>
-        <h1>About</h1>
-        <div>
-          LoggedInUser
-          <UserContext.Consumer>
-            {({loggedInUser})=><h1 className="text-lg font-bold">{loggedInUser}</h1>}
-          </UserContext.Consumer>
-        </div>
-        <User name={"Mahadev Athani"}/> 
-        {/* <UserClass name={"First (class)"} location={"First class"} /> */}
- 
+        LoggedInUser: <h1 className="text-lg font-bold">{loggedInUser}</h1>
       </div>
-    );
-  }
-}
-
+      <User name={"Mahadev Athani"} />
+      {/* <UserClass name={"First (class)"} location={"First class"} /> */}
+    </div>
+  );
+};
 
 export default About;
-
-
-
-
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
@@ -13,84 +12,59 @@ import { Provider } from "react-redux";
 import store from "./Redux/store";
 import Cart from "./components/Cart";
 
-//app chunking
-//code splitting
-//dyamic budling
-//lazy loading-to optimize
-//on demand loading
-
+// Lazy-loaded components
 const Grocery = lazy(() => import("./components/Grocery"));
 const About = lazy(() => import("./components/About"));
 
 const AppLayout = () => {
-  ///authentiaction code/logic
+    const [userName, setUserName] = useState();
 
-  const [userName, setUserName] = useState(); //passed userName
-  // const [userId, setUserId] = useState();
-  useEffect(() => {
-    //make an api call and send username and password
-    const data = {
-      name: "Mahadev Athani",
-      id: 8977,
-    };
-    setUserName(data.name);
+    useEffect(() => {
+        const data = { name: "Mahadev Athani", id: 8977 };
+        setUserName(data.name);
+    }, []);
 
-  }, []);
-  return (
-    /**Providing value to whole app */
-    <Provider store={store}>
-      <UserContext.Provider value={{ loggedInUser: userName, setUserName }}>
-        <div className="app">
-          <Header />
-          <Outlet />
-        </div>
-      </UserContext.Provider>
-    </Provider>
-  );
+    return (
+        <Provider store={store}>
+            <UserContext.Provider value={{ loggedInUser: userName, setUserName }}>
+                <div className="app">
+                    <Header />
+                    <Outlet />
+                </div>
+            </UserContext.Provider>
+        </Provider>
+    );
 };
 
 const appRouter = createBrowserRouter([
-  {
-    path: "/",
-    element: <AppLayout />,
-    children: [
-      {
+    {
         path: "/",
-        element: <Body />,
-      },
-      {
-        path: "/about",
-        element: (
-          <Suspense fallback={<>ON LOADING.....</>}>
-            <About />
-          </Suspense>
-        ),
-      },
-      {
-        path: "/contact",
-        element: <Contact />,
-      },
-      {
-        path: "/grocery",
-        element: (
-          <Suspense fallback={<>Loading.....</>}>
-            <Grocery />
-          </Suspense>
-        ),
-      },
-      {
-        path: "/restaurant/:resId",
-        element: <RestaurantMenu />,
-      },
-      {
-        path: '/cart',
-        element: <Cart />,
-      },
-    ],
-    errorElement: <Error />,
-  },
+        element: <AppLayout />,
+        children: [
+            { path: "/", element: <Body /> },
+            {
+                path: "/about",
+                element: (
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <About />
+                    </Suspense>
+                ),
+            },
+            {
+                path: "/grocery",
+                element: (
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <Grocery />
+                    </Suspense>
+                ),
+            },
+            { path: "/contact", element: <Contact /> },
+            { path: "/restaurant/:resId", element: <RestaurantMenu /> },
+            { path: "/cart", element: <Cart /> },
+        ],
+        errorElement: <Error />,
+    },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 root.render(<RouterProvider router={appRouter} />);
